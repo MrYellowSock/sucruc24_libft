be7f1f0f119133bec2d9bf12af0bd2b48ddc7c18
Suppanut Kulkamthorn <skulkamt@z2t3c1.42bangkok.com>
2023-03-11 17:45:38 +0700

    no more mem aloc

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: skulkamt <skulkamt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:07:04 by skulkamt          #+#    #+#             */
-/*   Updated: 2023/02/23 17:08:15 by skulkamt         ###   ########.fr       */
+/*   Updated: 2023/03/11 17:44:10 by skulkamt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 void	ft_putnbr_fd(int input, int fd)
 {
-	char	*conv;
-
-	conv = ft_itoa(input);
-	ft_putstr_fd(conv, fd);
-	free(conv);
+	if (input == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (input < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-input, fd);
+	}
+	else if (input < 10)
+		ft_putchar_fd(input + '0', fd);
+	else
+	{
+		ft_putnbr_fd(input / 10, fd);
+		ft_putnbr_fd(input % 10, fd);
+	}
 }
